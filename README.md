0a16634502c7151bf79e8d19a5cfdcbfb04e6c29
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-17 23:29:24 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Using a Custom Annotation
+# Introduction to Reflection
 
-With our @Doc annotation written, we can now use it like any other. 
+We take one more sidestep during this course, this time to what's called reflection. 
 
-In this section, we'll apply the annotation to the MathUtils class and a few of its methods.
+Reflection is a programming technique that allows us to examine (and even modify) the structure of objects at run-time. 
+
+During this section, we'll practice some basic reflection so that we can utilize these techniques to check for the presence of our @Doc annotation.
--- src/com/franktran/docgen/Main.java
+++ src/com/franktran/docgen/Main.java
@@ -1,8 +1,28 @@
 package com.franktran.docgen;
 
+import com.franktran.math.MathUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: Process the MathUtils class's annotations
-
+        // Get a class object
+        Class<?> clazz = MathUtils.class;
+        // Get all declared methods
+        Method[] methods = clazz.getDeclaredMethods();
+        // Loop over methods
+        for (Method method : methods) {
+            // Display method name
+            System.out.printf("name: %s%n", method.getName());
+            // Display parameter count
+            System.out.printf("\tparams: %s%n", method.getParameterCount());
+            // Display return type
+            System.out.printf("\treturn type: %s%n", method.getReturnType().getSimpleName());
+            // Display modifiers
+            int mods = method.getModifiers();
+            String modStr = Modifier.toString(mods);
+            System.out.printf("\tmodifiers: %s%n", modStr);
+        }
     }
 }
